b153af829fe4048b42b328ae637a75def3a703da
Gaurav Kumar <103420886+gauravai2025@users.noreply.github.com>
2026-02-23 22:18:11 +0530

    Time: 22 ms (15.76%) | Memory: 94.7 MB (16.55%) - LeetSync

---
+++ matrix/2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,20 @@
+// Approach-2 (Using simply math observation)
+// T.C : O(m*n)
+// S.C : O(1) //no including space of result
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        vector<vector<int>> result(m, vector<int>(n));
+        int l = original.size();
+
+        if (m * n != l) {
+            return {};
+        }
+
+        for (int i = 0; i < l; i++) {
+            result[i / n][i % n] = original[i];
+        }
+
+        return result;
+    }
+};
